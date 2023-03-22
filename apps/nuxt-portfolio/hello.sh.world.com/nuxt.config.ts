cbc12434403266941f5ca86bc9775d8598a8253b
msh <job.maengsh@gmail.com>
2023-03-22 18:20:29 +0900

    nuxt project 셋팅

--- apps/nuxt-portfolio/hello.sh.world.com/nuxt.config.ts
+++ apps/nuxt-portfolio/hello.sh.world.com/nuxt.config.ts
@@ -1,4 +1,46 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
+import eslintPlugin from "vite-plugin-eslint";
 
-})
+export default defineNuxtConfig({
+  css: ["~/assets/css/base.scss"],
+  buildModules: ["nuxt-vite", "@nuxtjs/style-resources"],
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: "ko",
+      },
+      title: "msh",
+      meta: [{ charset: "utf-8" }],
+    },
+  },
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          sourceMap: true,
+          additionalData: `
+                          @import "@/assets/css/common/variables.scss";
+                          @import "@/assets/css/common/mixins.scss"; 
+                      `,
+        },
+      },
+    },
+    // '@fortawesome/fontawesome-svg-core/styles.css',
+    modules: [
+      [
+        "@fullpage/nuxt-fullpage",
+        {
+          activeSection: 0,
+          mouseWheelSensitivity: 120,
+          showIndicators: false,
+        },
+      ],
+    ],
+    plugins: [
+      eslintPlugin({
+        failOnError: false,
+        cache: false,
+      }),
+      //   { src: "~/plugins/aos.js", mode: "client" },
+    ],
+  },
+});
